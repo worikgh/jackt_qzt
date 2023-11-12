41feb5992933cf07ba0a0b6038324d24935a588b
Worik <5387413+worikgh@users.noreply.github.com>
2023-11-12 02:49:07 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,136 @@
+//! Takes an audio input and output it to an audio output.
+//! Makes a copy of the audio in a file
+//! Three commandline arguments:
+//! 1. In port name
+//! 2. Out port name
+//! 3. String holing path for output file
+//! All JACK notifications are also printed out.
+use clap::Parser;
+use std::io;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short = 'i', long)]
+    port_in: String,
+    #[arg(short = 'o', long)]
+    port_out: String,
+    #[arg(short, long)]
+    file_name: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let port_in: String = args.port_in;
+    let port_out: String = args.port_out;
+
+    // Create client
+    let (client, _status) =
+        jack::Client::new("rust_jack_simple", jack::ClientOptions::NO_START_SERVER).unwrap();
+
+    // Register ports. They will be used in a callback that will be
+    // called when new data is available.
+    let in_a = client
+        .register_port(port_in.as_str(), jack::AudioIn)
+        .unwrap();
+    let mut out_a = client
+        .register_port(port_out.as_str(), jack::AudioOut)
+        .unwrap();
+    let process_callback = move |_: &jack::Client, ps: &jack::ProcessScope| -> jack::Control {
+        let out_a_p = out_a.as_mut_slice(ps);
+        let in_a_p = in_a.as_slice(ps);
+        out_a_p.clone_from_slice(in_a_p);
+        jack::Control::Continue
+    };
+    let process = jack::ClosureProcessHandler::new(process_callback);
+
+    // Activate the client, which starts the processing.
+    let active_client = client.activate_async(Notifications, process).unwrap();
+
+    // Wait for user input to quit
+    println!("Press enter/return to quit...");
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input).ok();
+
+    active_client.deactivate().unwrap();
+}
+
+struct Notifications;
+
+impl jack::NotificationHandler for Notifications {
+    fn thread_init(&self, _: &jack::Client) {
+        println!("JACK: thread init");
+    }
+
+    fn shutdown(&mut self, status: jack::ClientStatus, reason: &str) {
+        println!("JACK: shutdown with status {status:?} because \"{reason}\"",);
+    }
+
+    fn freewheel(&mut self, _: &jack::Client, is_enabled: bool) {
+        println!(
+            "JACK: freewheel mode is {}",
+            if is_enabled { "on" } else { "off" }
+        );
+    }
+
+    fn sample_rate(&mut self, _: &jack::Client, srate: jack::Frames) -> jack::Control {
+        println!("JACK: sample rate changed to {srate}");
+        jack::Control::Continue
+    }
+
+    fn client_registration(&mut self, _: &jack::Client, name: &str, is_reg: bool) {
+        println!(
+            "JACK: {} client with name \"{}\"",
+            if is_reg { "registered" } else { "unregistered" },
+            name
+        );
+    }
+
+    fn port_registration(&mut self, _: &jack::Client, port_id: jack::PortId, is_reg: bool) {
+        println!(
+            "JACK: {} port with id {}",
+            if is_reg { "registered" } else { "unregistered" },
+            port_id
+        );
+    }
+
+    fn port_rename(
+        &mut self,
+        _: &jack::Client,
+        port_id: jack::PortId,
+        old_name: &str,
+        new_name: &str,
+    ) -> jack::Control {
+        println!("JACK: port with id {port_id} renamed from {old_name} to {new_name}",);
+        jack::Control::Continue
+    }
+
+    fn ports_connected(
+        &mut self,
+        _: &jack::Client,
+        port_id_a: jack::PortId,
+        port_id_b: jack::PortId,
+        are_connected: bool,
+    ) {
+        println!(
+            "JACK: ports with id {} and {} are {}",
+            port_id_a,
+            port_id_b,
+            if are_connected {
+                "connected"
+            } else {
+                "disconnected"
+            }
+        );
+    }
+
+    fn graph_reorder(&mut self, _: &jack::Client) -> jack::Control {
+        println!("JACK: graph reordered");
+        jack::Control::Continue
+    }
+
+    fn xrun(&mut self, _: &jack::Client) -> jack::Control {
+        println!("JACK: xrun occurred");
+        jack::Control::Continue
+    }
+}
